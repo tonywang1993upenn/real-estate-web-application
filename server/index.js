fa2c30ded129386ce10f64b0ea9b353f0942d024
tonywang <xinyuwa@seas.upenn.edu>
2024-01-22 23:31:22 -0800

    Create a server and connect to a database

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(
+    () => { console.log("Connected to MongoDB") })
+.catch(
+    (err) => { console.log(err); })
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is runing on port 3000!!')
+})
